b3b068dec4cd6f36644114361730f1a0dc3be194
Felipe Reyes <freyes@tty.cl>
2014-09-01 13:10:39 +0200

    awesome: use a path in the home for the wallpaper

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/home/freyes/.config/awesome/
 theme.titlebar_maximized_button_normal_active = "/home/freyes/.config/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/home/freyes/.config/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/freyes/.config/awesome/themes/default/background.png"
+theme.wallpaper = "/home/freyes/.background.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/home/freyes/.config/awesome/themes/default/layouts/fairhw.png"
