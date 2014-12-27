10166fd13dbbb1b90a4d283457630821e997d3e9
Felipe Reyes <felipe.reyes@canonical.com>
2014-12-27 20:23:19 -0300

    [bootstrap] link awesome wm config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+set -x
 CURDIR=$(readlink -f $(dirname $0))
 
 test -e $HOME/.emacs || ln -s $CURDIR/dot_emacs $HOME/.emacs
 test -e $HOME/.gitconfig || ln -s $CURDIR/gitconfig $HOME/.gitconfig
 test -e $HOME/.hgrc || ln -s $CURDIR/hgrc $HOME/.hgrc
 test -e $HOME/.muttrc || ln -s $CURDIR/muttrc $HOME/.muttrc
+test -e $HOME/.config/awesome || ln -s $CURDIR/awesome $HOME/.config/awesome
